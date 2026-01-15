22d87756c73763086bb59084e61a093892bfc590
liamgines <109954426+liamgines@users.noreply.github.com>
2026-01-15 15:44:48 -0800

    Create a window

--- main.c
+++ main.c
@@ -9,7 +9,22 @@
 #define _UNICODE
 #endif
 
+LRESULT CALLBACK WindowProcedure(HWND window, UINT message, WPARAM wParameter, LPARAM lParameter) {
+	return DefWindowProc(window, message, wParameter, lParameter);
+}
+
+// Create a window!
 int WINAPI wWinMain(HINSTANCE appInstance, HINSTANCE previousInstance, PWSTR commandLine, int visibility) {
-	int selection = MessageBox(NULL, L"Hello", L"Message", MB_OK);
+	int errorCode;
+	WNDCLASS windowClass = { 0 };
+	windowClass.hInstance = appInstance;
+	windowClass.lpszClassName = L"Window Class";
+	windowClass.lpfnWndProc = WindowProcedure;
+	RegisterClass(&windowClass);
+
+	CreateWindow(windowClass.lpszClassName, L"Window", WS_OVERLAPPEDWINDOW,
+				 CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+				 NULL, NULL, appInstance, NULL);
+	errorCode = GetLastError();
 	return 0;
 }
